c1e0a940e548fc7a77600948c0152deea46806c5
KKingmo <55ooccmm@gmail.com>
2022-03-14 18:26:02 +0900

    첫 잔디

---
+++ class/pages/01-01-qqq/aaa/bbb/index.js
@@ -0,0 +1,11 @@
+export default function AAAPage2() {
+    // 여기는 자바스크립트 쓰는 곳
+
+    return (
+        <div>
+            안녕하세요 bbb index.js 입니다.
+            <br />
+            <input type="text" />
+        </div>
+    );
+}
---
+++ freeboard_frontend/pages/boards/new/index.js
@@ -0,0 +1,21 @@
+import { MyTitle, MyEmail, MyLabel } from "../../../styles/emotion.js";
+
+export default function AAAPage() {
+    return (
+        <MyTitle>
+            로그인
+            <MyLabel>아이디</MyLabel>
+            <MyEmail type="text" />
+            <MyLabel>비밀번호</MyLabel>
+            <MyEmail type="text" />
+        </MyTitle>
+    );
+
+    // return (
+    //     <MyTitle>
+    //         로그인
+    //         <MyEmail type="text" />
+    //         <img src="/aaa/lotto.png" />
+    //     </MyTitle>
+    // );
+}
